3eabf3b4742cfd30abae2c8826ff6a167a0fb8b4
MTrilles <trillesmark11@gmail.com>
2026-09-19 20:29:06 +0800

    Small Update 7: Logging days set to 1 day

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
@@ -69,7 +69,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'max_files' => env('LOG_DAILY_DAYS', 14),
+            'max_files' => env('LOG_DAILY_DAYS', 1),
             'replace_placeholders' => true,
         ],
 
